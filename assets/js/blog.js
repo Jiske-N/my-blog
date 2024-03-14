d633d3cfe7c8d649fdd64a63128e7b8f7fb20e2d
@r0f33 <jiske.nicolson@gmail.com>
2024-03-14 21:38:08 +1030

    added all remaining links, commented and formatted, troubleshooted errors.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,41 +1,17 @@
-const blog = document.getElementById('blogposts');
+// const blog = document.getElementById('blogposts');
 const backButton = document.getElementById('button');
 
-const createArticle = document.createElement('article');
-const createH4 = document.createElement('h4');
-const createP = document.createElement('p');
-const createAside = document.createElement('aside');
+// const createArticle = document.createElement('article');
+// const createH4 = document.createElement('h4');
+// const createP = document.createElement('p');
+// const createAside = document.createElement('aside');
 
-// _______________________________________________________________________________________________
-
+// navigate back to blog form
 backButton.addEventListener('click', function () {
-    window.location='file:///C:/Users/jiske/Bootcamp/Week%204/my-blog/index.html';
+    window.location = './index.html';
 });
 
-// ___________________________________________________________________________________________________
-
-// function renderPost() {
-//     // const blogPosts = retrievePost();
-//     const blogPosts = JSON.parse(localStorage.getItem('post'));
-
-//     for (let index = 0; index < blogPosts.length; index++) {
-   
-//         blog.appendChild(createArticle);
-//         createArticle.appendChild(createH4, createP, createAside);
-//         createArticle.appendChild(createP);
-//         createArticle.appendChild(createAside);
-    
-//         console.log(blogPosts);
-//         createH4.textContent = blogPosts[index].title;
-//         createP.textContent = blogPosts[index].content;
-//         createAside.textContent = (`Posted by: ${blogPosts[index].username}`);
-//     }
-// };
-
-// renderPost();
-
-// __________________________________________________________________________________________________________
-
+// create elements and display posts from local storage
 function renderPost() {
     const blogPosts = JSON.parse(localStorage.getItem('post'));
     const blog = document.getElementById('blogposts');
